3efaae66947bde9598101938c2f27751ed7e34ca
Victor Cabezas <vcabezas@tuenti.com>
2023-06-15 12:51:10 +0200

    Adjust plugin type

--- types/plugin.pp
+++ types/plugin.pp
@@ -1,5 +1,5 @@
 type Fluentbit::Plugin = Struct[{
   plugin_name          => String[1],
-  type                 => String[1],
+  Optional[type]       => Enum['input', 'output', 'filter'],
   Optional[properties] => Hash,
 }]
