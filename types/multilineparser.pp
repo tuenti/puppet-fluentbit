d7fe7c2a63c02188d08212b418ca9ab6753b027c
Victor Cabezas <vcabezas@tuenti.com>
2023-06-28 14:11:52 +0200

    Use rules for array of rules

--- types/multilineparser.pp
+++ types/multilineparser.pp
@@ -1,6 +1,6 @@
 type Fluentbit::MultilineParser = Struct[{
   type                    => Enum['regex'],
-  rule                    => Array[Fluentbit::MultilineParserRule],
+  rules                   => Array[Fluentbit::MultilineParserRule],
   Optional[parser]        => String[1],
   Optional[key_content]   => String[1],
   Optional[flush_timeout] => String[1],
